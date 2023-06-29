95d19fb48c58cb121936d4a530bd923ffda59006
César <cesarlpb@gmail.com>
2023-06-29 18:30:08 +0200

    Creamos componente Footer

--- primer-react/src/App.jsx
+++ primer-react/src/App.jsx
@@ -1,18 +1,24 @@
+// Importaciones de funciones, hojas de estilo, img, etc.
 import { useState } from 'react'
 import reactLogo from '/react.svg'
 import viteLogo from '/vite.svg'
 import meme from '/img/meme.png'
 import './App.css'
+import './Footer'
+import Footer from './Footer'
 
-const MiBoton = () => <button onClick={() => console.log("Click!")}>Click me</button>
+// Definimos nuestras funciones
+export const MiBoton = () => <button onClick={() => console.log("Click!")}>Click me</button>
 
 const MyAwesomeDiv = () => {
   return (<div>My div is amazin' bruh</div>)
 }
 
+// función principal del componente que es el principal "return" del component y se llaman igual:
 function App() {
   const [count, setCount] = useState(0)
   return (
+    // Fragmento
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -35,12 +41,18 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      {/* Meme */}
       <div>
         <img src={meme} alt="meme" width={500}/>
+        <br />
         <MiBoton/>
       </div>
+
+      {/* Usamos Footer */}
+      <Footer/>
     </>
   )
 }
-
+// Es necesario colocar export de la función principal, en este caso App, para que podamos utilizarla desde otro archivo
 export default App
---
+++ primer-react/src/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer(){
+  return(
+    <>
+    <div className="footer">
+      <p>Esto es un Footer</p>
+      <p>&copy; 2023 - React cosas </p>
+    </div>
+    </>
+  )
+}
+export default Footer
