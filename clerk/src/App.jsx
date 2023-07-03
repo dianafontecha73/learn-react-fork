722f7ca8f3eb3d8b6fcc8e3e17a3ed4a043ebc9a
César <cesarlpb@gmail.com>
2023-07-03 16:46:48 +0200

    Modificaciones para React con Vite

--- clerk/src/App.jsx
+++ clerk/src/App.jsx
@@ -1,15 +1,22 @@
+import React from "react";
 import './App.css'
+import { ClerkProvider } from "@clerk/clerk-react";
+
+if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
 
 function App() {
 
   return (
     <>
-      
-      <h1>Clerk</h1>
-      <div><a href='#'>Registro</a></div>
-      <br />
-      <div><a href='#'>Login</a></div>
-
+      <ClerkProvider publishableKey={clerkPubKey}>
+        <h1>Clerk</h1>
+        <div><a href='#'>Registro</a></div>
+        <br />
+        <div><a href='#'>Login</a></div>
+      </ClerkProvider>
     </>
   )
 }
