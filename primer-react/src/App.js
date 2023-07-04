6f092e2b67e8d843182e46a04d7f401cd25858d1
César <cesarlpb@gmail.com>
2023-07-04 10:27:21 +0200

    Añadimos componente Footer con sus estilos

--- primer-react/src/App.js
+++ primer-react/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 
+// Componente Footer
+function Footer() {
+  // lógica del componente
+  return (
+    <footer id='footer'>
+      <p>Este es el pie de página desde el componente.</p>
+    </footer>
+  )
+}
+
 function App() {
   return (
     <>
     <div className='container'>
       <h1>Primer React</h1>
       <p>Este es el componente <code>App</code>.</p>
+      <Footer />
     </div>
     </>
   );
