2e602017d297a32d8a0f5e72cf57ebd91c662b9d
César <cesarlpb@gmail.com>
2023-07-04 12:33:27 +0200

    Twitter clone v0.Código inicial con estilos y logo

---
+++ twitter-clone-0/src/App.js
@@ -0,0 +1,16 @@
+import logo from './assets/img/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Twitter Clone v0</h1>
+        <img src={logo} width={300} className="App-logo" alt="logo" />
+        <p>Cosas de Twitter clone</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
