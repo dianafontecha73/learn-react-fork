5aaa3e0276746785091ee61049e748025db7dd1d
César <cesarlpb@gmail.com>
2023-07-05 11:50:52 +0200

    Reorganizamos <Layout>, navbar y header. Estilos.

--- twitter-clone-1/src/App.js
+++ twitter-clone-1/src/App.js
@@ -8,10 +8,6 @@ import { Routes, Route, Outlet, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Twitter Clone v1</h1>
-        <img src={logo} width={300} className="App-logo" alt="logo" />
-      </header>
       {/* Routes nest inside one another. Nested route paths build upon
         parent route paths, and nested route elements render inside
         parent route elements. See the note about <Outlet> below. */}
@@ -27,30 +23,18 @@ function App() {
         </Route>
       </Routes>
       
-    {/* TODO: Crear un componente <Main> */}
-    <main>
-      <div className="container">
-        <h2>Últimos Tweets</h2>
-        <div className="tweet-container">
-          {/* key: Cuando hacemos listas en React, se debe colocar un atributo "key" que sea único para que React pueda actualizar bien lo campos. Se suele usar un id. */}
-          {tweets.map((tweet) => (
-            <Tweet key={tweet.id} {...tweet} />
-          ))}
-        </div>
-      </div>
-    </main>
     </div>
   );
 }
-
-function Layout() {
+function Header(){
   return (
-    <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
+    <header className="App-header">
+      <div className='brand'>
+        <h1>Twitter Clone v1</h1>
+        <img src={logo} width={300} className="App-logo" alt="logo" />
+      </div>
       <nav>
-        <h2>Menú</h2>
-        <ul>
+        <ul className='navbar'>
           <li>
             <Link to="/">Inicio</Link>
           </li>
@@ -65,38 +49,73 @@ function Layout() {
           </li>
         </ul>
       </nav>
+    </header>
+  )}
+function Layout() {
+  return (
+    <div className="layout">
+      {/* A "layout route" is a good place to put markup you want to
+        share across all the pages on your site, like navigation. 
+      
+      Este componente aparece en todas las páginas  
+      */}
 
-      <hr />
+      <Header/>
 
       {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
+        so you can think about this <Outlet> as a placeholder for
+        the child routes we defined above. 
+      
+        El Outlet es el componente que se encarga de renderizar el contenido de las rutas hijas. -> <Home>, <About>, <Dashboard>, <NoMatch>, etc.
+        */}
       <Outlet />
+
+      <Footer/>
     </div>
   );
 }
 
+function Footer(){
+  return (
+    <footer className='footer'>
+      <p>Este es el footer de Layout</p>
+    </footer>
+  )
+}
 function Home() {
   return (
     <div>
-      <h2>Home</h2>
-      <p>Esto es el home</p>
+      {/* TODO: Crear un componente <Main> */}
+      <main>
+        <h2>Home</h2>
+        <p style={{color: "white"}}>Esto es el home</p>
+        <div className="container">
+          <h2>Últimos Tweets</h2>
+          <div className="tweet-container">
+            {/* key: Cuando hacemos listas en React, se debe colocar un atributo "key" que sea único para que React pueda actualizar bien lo campos. Se suele usar un id. */}
+            {tweets.map((tweet) => (
+              <Tweet key={tweet.id} {...tweet} />
+            ))}
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
 
 function About() {
   return (
-    <div>
+    <div className='about'>
       <h2>About</h2>
       <p>Esto es el about</p>
+      <p>Acerca de</p>
     </div>
   );
 }
 
 function Dashboard() {
   return (
-    <div>
+    <div className='dashboard'>
       <h2>Dashboard</h2>
       <p>Esto es el dashboard</p>
     </div>
@@ -105,7 +124,7 @@ function Dashboard() {
 
 function NoMatch() {
   return (
-    <div>
+    <div className='nomatch'>
       <h2>Error 404</h2>
       <p>
         Vaya, no pudimos encontrar lo que estabas buscando. ¿Quieres <Link to="/">Volver a Home</Link>?
