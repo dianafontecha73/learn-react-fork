e979cd2037262b942703f41df79f56eb381b4457
César <cesarlpb@gmail.com>
2023-07-05 13:13:00 +0200

    Añadimos ClerkProvider

--- twitter-clone-1/src/index.js
+++ twitter-clone-1/src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// React Router DOM
 import { BrowserRouter } from "react-router-dom";
+// Clerk
+import { ClerkProvider } from "@clerk/clerk-react";
 
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("No se ha encontrado la contraseña de Clerk. Revisa el .env")
+}
+
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* TODO: Añadir localización esES -> guía: https://clerk.com/docs/nextjs/localization */}
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </React.StrictMode>
 );
 
