2b6e05cf036b93910a35e081b62e75d26abe8ce0
César <cesarlpb@gmail.com>
2023-07-05 09:26:47 +0200

    Creamos componente <Tweet> en App

--- twitter-clone-1/src/App.js
+++ twitter-clone-1/src/App.js
@@ -6,24 +6,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Twitter Clone v0</h1>
+        <h1>Twitter Clone v1</h1>
         <img src={logo} width={300} className="App-logo" alt="logo" />
         <div>
           <h2>Últimos Tweets</h2>
           <div className="tweet-container">
             {/* Cuando hacemos listas en React, se debe colocar un atributo "key" que sea único para que React pueda actualizar bien lo campos. Se suele usar un id. */}
-            {tweets.map((tweet) => (
-              <div className="tweet-item" key={tweet.id}>
-                <p className="tweet-field id">#{tweet.id}</p>
-                <p className="tweet-field content">{tweet.content}</p>
-                <p className="tweet-field author">{tweet.created_on} por <a href="#">@{tweet.author}</a></p>
-              </div>
+            {tweets.map(({id, content, author, created_on}) => (
+              <Tweet id={id} content={content} created_on={created_on} author={author} />
             ))}
           </div>
         </div>
       </header>
     </div>
   );
+}
+
+function Tweet(props){
+  const {id, content, created_on, author} = props;
+  return (
+    <div className="tweet-item" key={id}>
+      <p className="tweet-field id">{id}</p>
+      <p className="tweet-field content">{content}</p>
+      <p className="tweet-field author">{created_on} por <a href="#">@{author}</a></p>
+    </div>
+  )
 }
 
 export default App;
