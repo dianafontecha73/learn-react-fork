bd7747dcbcfed688576553959a5c809dd6188968
César <cesarlpb@gmail.com>
2023-07-05 20:04:57 +0200

    Dato del usuario logeado en <User> -> /about

--- twitter-clone-1/src/App.js
+++ twitter-clone-1/src/App.js
@@ -105,10 +105,64 @@ function Home() {
   );
 }
 
+const User = () => {
+  // Use the useUser hook to get the Clerk.user object
+  const { isLoaded, isSignedIn, user } = useUser()
+
+  if (!isLoaded || !isSignedIn) {
+    return null
+  }
+
+  console.log(user)
+
+  return (<table style={{ margin: "0 auto"}}>
+    <tr>
+      <th>Propiedad</th>
+      <th>Dato</th>
+    </tr>
+    <tr>
+      <td>id:</td>
+      <td>{user.id}</td>
+    </tr>
+    <tr>
+      <td>username:</td>
+      <td>{user.username}</td>
+    </tr>
+    <tr>
+      <td>firstname:</td>
+      <td>{user.firstName}</td>
+    </tr>
+    <tr>
+      <td>lastname:</td>
+      <td>{user.lastName}</td>
+    </tr>
+    <tr>
+      <td>fullname:</td>
+      <td>{user.fullName}</td>
+    </tr>
+    <tr>
+      <td>emails:</td>
+      <td>{user.emailAddresses.map( (email) => email.emailAddress )}</td>
+    </tr>
+    <tr>
+      <td>profileImageUrl:</td>
+      <td>
+        <img src={user.profileImageUrl} width={150} />
+        <br/>
+        <a href={user.profileImageUrl} target='_blank'>{user.profileImageUrl}</a>
+        </td>
+    </tr>
+  </table>
+  )
+}
+
 function About() {
   return (
     <div>
       <h2>About</h2>
+      <p>Datos del usuario que ha iniciado sesión</p>
+      <h2>User:</h2>
+      <User />
     </div>
   );
 }
