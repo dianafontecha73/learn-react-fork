27e006ca2eb2fae9d61c7f96c53a7ccdeebdde80
César <cesarlpb@gmail.com>
2023-07-06 16:52:33 +0200

    Twitter Clone v2 - código inicial

--- requests/fetch.js
+++ requests/fetch.js
@@ -1,7 +1,7 @@
 // baseUrl
-
+const baseUrl = "http://79.143.92.203:3000/api/cdm/tweets"
 // GET de Lista
-fetch("http://79.143.92.203:3000/api/cdm/tweets").then(res => res.json()).then(res => console.log(res))
+fetch(baseUrl).then(res => res.json()).then(res => console.log(res))
 // GET de un objeto
-fetch("http://79.143.92.203:3000/api/cdm/tweets/1").then(res => res.json()).then(res => console.log(res))
+fetch(`${baseUrl}/1`).then(res => res.json()).then(res => console.log(res))
 // TODO: pasar cada uno de estos fetch a una function
