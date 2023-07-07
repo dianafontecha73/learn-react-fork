fe396d559be0482987d957c74d31fa59ee3f8b97
César <cesarlpb@gmail.com>
2023-07-07 09:52:16 +0200

    Añadimos estado title y fn handleTitle

--- examen-3-cen/src/App.js
+++ examen-3-cen/src/App.js
@@ -1,8 +1,25 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [title, setTitle] = useState(''); // Estado para el título con hook useState
+  const [content, setContent] = useState(''); // Estado para el contenido con hook useState
+  const [tags, setTags] = useState(''); // Estado para las etiquetas con hook useState
+
+  // Función que escribe el título en consola y actualiza el estado
+  const handleTitle = (e) => {
+    // Actualiza el estado
+    setTitle(e.target.value); // e.target.value es el valor del input
+    console.log(e.target.value);
+  }
+
+  // TODO:
+  // handleContent
+  // handleTags
+
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <h1>Crear Post</h1>
@@ -11,10 +28,10 @@ function App() {
         <form>
           
           <label>Titulo</label>
-          <input type="text" placeholder="Titulo" />
+          <input type="text" placeholder="Titulo" onChange={handleTitle} />
           <br/>
           <label>Contenido</label>
-          <textarea placeholder="Contenido">Escribe el contenido...</textarea>
+          <textarea placeholder="Contenido" defaultValue="Escribe el contenido..."></textarea>
           <br/>
           <label>Etiquetas</label>
           <input type="text" placeholder="Etiquetas separadas por comas" />
@@ -23,6 +40,13 @@ function App() {
         </form>
       </div>
     </div>
+    <div>
+      <h2>Datos</h2>
+      <p>Titulo: {title}</p>
+      <p>Contenido: {content}</p>
+      <p>Etiquetas: {tags}</p>
+    </div>
+    </>
   );
 }
 
