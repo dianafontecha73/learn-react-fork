c8e2852026a4fb1a80bdaf26c17c9e685e32789b
César <cesarlpb@gmail.com>
2023-07-07 09:26:40 +0200

    Ejercicio 2 - código inicial

--- examen-3-cen/src/App.js
+++ examen-3-cen/src/App.js
@@ -5,19 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Crear Post</h1>
       </header>
+      <div className="container">
+        <form>
+          
+          <label>Titulo</label>
+          <input type="text" placeholder="Titulo" />
+          <br/>
+          <label>Contenido</label>
+          <textarea placeholder="Contenido">Escribe el contenido...</textarea>
+          <br/>
+          <label>Etiquetas</label>
+          <input type="text" placeholder="Etiquetas separadas por comas" />
+        </form>
+      </div>
     </div>
   );
 }
