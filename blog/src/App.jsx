9aab05440aaa33e91b75a376584c48261681a079
César <cesarlpb@gmail.com>
2023-07-13 19:02:15 +0200

    Empezamos a crear el componente <Home>

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,28 +1,22 @@
 // import { useState } from 'react'
 import logo from '/favicon.png'
 import './App.css'
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
+
+const Home = () => {
+  return (
+  <>
+  <div id="home" className='border border-dark'>
+    <h1>Home</h1>
+  </div>
+  </>
+)};
 
 function App() {
 
   return (
     <>
-      <h1>Blog</h1>
-      <div className="card">
-        <p>¡Bienvenid@ a mi Blog!</p>
-        <img src={logo} className="d-inline-block mx-auto" width={150}/>
-      </div>
-      <div>
-        <Button variant="primary">Primary</Button>{' '}
-        <Button variant="secondary">Secondary</Button>{' '}
-        <Button variant="success">Success</Button>{' '}
-        <Button variant="warning">Warning</Button>{' '}
-        <Button variant="danger">Danger</Button>{' '}
-        <Button variant="info">Info</Button>{' '}
-        <Button variant="light">Light</Button>{' '}
-        <Button variant="dark">Dark</Button>
-        <Button variant="link">Link</Button>
-      </div>
+      <Home />
     </>
   )
 }
