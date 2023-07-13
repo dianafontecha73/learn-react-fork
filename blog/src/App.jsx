1b6775293a7b2bc6d6aaed4d9c73265c8692842c
César <cesarlpb@gmail.com>
2023-07-13 20:08:35 +0200

    Componente <Hero>

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -10,7 +10,9 @@ const Home = () => {
     <header>
       <Navbar />
     </header>
-    
+    <main>
+      <Hero />
+    </main>
   </div>
   </>
 )};
@@ -34,11 +36,25 @@ const Navbar = () => {
   </>
 )}
 const Hero = () => {
+  // Fuente: https://mdbootstrap.com/docs/react/extended/hero/
   return (
   <>
-  <div className='border border-danger'>
-    <p>Hero</p> 
-  </div>
+  <div
+    className='p-0 text-center bg-image border-danger'
+    style={{ backgroundImage: "url('https://mdbootstrap.com/img/new/slides/041.webp')", height: 400 }}
+      >
+      <div className='mask' style={{ height: '100%', width: '100%', backgroundColor: 'rgba(0, 0, 0, 0.45)' }}>
+        <div className='d-flex justify-content-center align-items-center h-100'>
+          <div className='text-white'>
+            <h1 className='mb-3'>Heading</h1>
+            <h4 className='mb-3'>Subheading</h4>
+            <a className='btn btn-outline-light btn-lg' href='#!' role='button'>
+              Call to action
+            </a>
+          </div>
+        </div>
+      </div>
+  </div> 
   </>
 )}
 const Posts = () => {
@@ -65,7 +81,6 @@ function App() {
   return (
     <>
       <Home />
-      <Navbar />
       <Hero />
       <Posts />
       <Footer />
