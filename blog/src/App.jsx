4ea3967a4b11e4c45499353699af4f222b3f45b7
César <cesarlpb@gmail.com>
2023-07-13 19:08:21 +0200

    Presentamos componentes Navbar, Hero, Posts,Footer

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -8,15 +8,51 @@ const Home = () => {
   <>
   <div id="home" className='border border-dark'>
     <h1>Home</h1>
+    {/* Colocar etiquetas aqui */}
   </div>
   </>
 )};
 
+const Navbar = () => {
+  return (
+  <>
+    <div>
+      <img src={logo} alt="logo :)" width={150} />
+      <h2>Navbar</h2>
+    </div> 
+  </>
+)}
+const Hero = () => {
+  return (
+  <>
+    <p>Hero</p> 
+  </>
+)}
+
+const Posts = () => {
+  return (
+  <>
+    <p>Posts</p> 
+    <p>Posts</p> 
+    <p>Posts</p> 
+  </>
+)}
+
+const Footer = () => {
+  return (
+  <>
+    <p>Footer</p> 
+  </>
+)}
 function App() {
 
   return (
     <>
       <Home />
+      <Navbar />
+      <Hero />
+      <Posts />
+      <Footer />
     </>
   )
 }
