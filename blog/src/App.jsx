cb1f847c0449aeaccba04fa54d4aca1b287d0964
César <cesarlpb@gmail.com>
2023-07-13 18:33:45 +0200

    Botones de Boostrap usando React-Bootstrsap

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 import logo from '/favicon.png'
 import './App.css'
+import Button from 'react-bootstrap/Button';
 
 function App() {
 
@@ -10,6 +11,17 @@ function App() {
       <div className="card">
         <p>¡Bienvenid@ a mi Blog!</p>
         <img src={logo} width={150}/>
+      </div>
+      <div>
+        <Button variant="primary">Primary</Button>{' '}
+        <Button variant="secondary">Secondary</Button>{' '}
+        <Button variant="success">Success</Button>{' '}
+        <Button variant="warning">Warning</Button>{' '}
+        <Button variant="danger">Danger</Button>{' '}
+        <Button variant="info">Info</Button>{' '}
+        <Button variant="light">Light</Button>{' '}
+        <Button variant="dark">Dark</Button>
+        <Button variant="link">Link</Button>
       </div>
     </>
   )
